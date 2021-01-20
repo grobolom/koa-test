ff2ba3bb1b5c90790744c8977b79b17eb8916c26
Vasja Volin <vasja.volin@gmail.com>
2021-01-19 23:02:02 -0800

    make some more garbage routes?

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+
 const db = require("../models");
+
 const User = db.user;
 
-app.use(async ctx => {
-  let admin = await User.findOne({ where: { username: 'admin' }});
-
-  if (!admin) {
-    admin = await User.create({ username: 'admin' });
-  }
-
-  ctx.body = admin.username;
+router.get('/', async (ctx, next) => {
+  // ctx.router ?
 });
+
+router.get('/videos', async (ctx, next) => {
+  ctx.body = await db.video.findAll();
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000, () => {
   console.log('listening on 3000');

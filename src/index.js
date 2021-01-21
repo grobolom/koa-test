359848147184104c9b7b79b2669be142bd938979
Vasja Volin <vasja.volin@gmail.com>
2021-01-21 14:45:41 -0800

    start fleshing out ingesting new videos

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const probe = require("node-ffprobe");
 
 const app = new Koa();
 const router = new Router();
 
 const db = require("../models");
+const koaBody = require('koa-body');
 
 const User = db.user;
-
-router.get('/', async (ctx, next) => {
-  // ctx.router ?
-});
+const Video = db.video;
 
 router.get('/videos', async (ctx, next) => {
-  ctx.body = await db.video.findAll({ include: ['video_details']});
+  ctx.body = await db.video.findAll({ limit: 10, include: ['video_details']});
+});
+
+router.post('/videos', async (ctx, next) => {
+  const { url, title, description, author } = ctx.request.body;
+  // need to validate that we have all of the above, skipping for now
+  const probe_data = await probe(url);
+  console.log(probe_data);
+
+  const duration = probe_data.streams[0].duration
+  const aspect_ratio = probe_data.streams[0].display_aspect_ratio
+
+  let video;
+
+  try {
+    video = await Video.create({
+      title,
+      description,
+      author,
+      duration,
+      source: url,
+      video_details: {
+        aspect_ratio,
+        ffprobe_metadata: probe_data,
+      }
+    }, { include: ['video_details']});
+  } catch(e) {
+    console.error(e);
+    ctx.body = e;
+  }
+
+  ctx.body = video;
 });
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
+  .use(koaBody())
+  .use(router.routes());
 
 app.listen(3000, () => {
   console.log('listening on 3000');
